dda60136eba5cbbdefd2f876de80d09738091a4b
MAMUN-1000 <mahmudurrahmanmamun01@gmail.com>
2025-09-02 15:06:54 +0600

    upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let password=document.getElementById('pass');
+let btn=document.getElementById('click');
+
+let count=0;
+
+if(password>=8 && password>='A'  && password<='Z'&& password>='a' && password>='z' ){
+    count++;
+   btn.addEventListener(Element =>{
+    Element.document.writeln=" Valid Password";
+   });
+}
+else{
+      btn.document.writeln.innerHTML="Invalid Password";  
+}
